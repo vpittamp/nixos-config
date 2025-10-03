bc65719cdf35ed04abc22dec9988fb90ae81164d
vpittamp <vpittamp@gmail.com>
2025-10-03 14:06:29 -0400

    Fix desktop entry shell escaping for speech-to-text shortcut

--- home-modules/desktop/speech-to-text-shortcuts.nix
+++ home-modules/desktop/speech-to-text-shortcuts.nix
@@ -2,7 +2,19 @@
 # Provides keyboard shortcuts for nerd-dictation toggle
 { config, lib, pkgs, ... }:
 
+let
+  # Wrapper script that sets up environment and calls nerd-dictation-toggle
+  toggleWrapper = pkgs.writeScriptBin "nerd-dictation-toggle-kde" ''
+    #!${pkgs.bash}/bin/bash
+    export PYTHONPATH=/var/lib/vosk-python:$PYTHONPATH
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+    exec /run/current-system/sw/bin/nerd-dictation-toggle
+  '';
+in
 {
+  # Add wrapper to user packages
+  home.packages = [ toggleWrapper ];
+
   programs.plasma.configFile."kglobalshortcutsrc" = {
     # Custom shortcuts for speech-to-text
     "speech-to-text" = {
@@ -16,6 +28,6 @@
     name = "Toggle Speech Dictation";
     key = "Ctrl+Space";
     comment = "Start/stop speech-to-text keyboard dictation";
-    command = "${pkgs.bash}/bin/bash -c 'export PYTHONPATH=/var/lib/vosk-python:$PYTHONPATH; export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH; /run/current-system/sw/bin/nerd-dictation-toggle'";
+    command = "${toggleWrapper}/bin/nerd-dictation-toggle-kde";
   };
 }
