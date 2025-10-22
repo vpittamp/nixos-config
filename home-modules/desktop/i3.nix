f0e3d69f0321f9064bf5b5f83a20cab67ce69aa5
vpittamp <vpittamp@gmail.com>
2025-10-22 07:01:26 -0400

    fix(i3): Remove all borders and decorations from fzf project switcher
    Applied i3-specific configuration to remove title bar and borders:
    
    i3 config:
    - Added 'border pixel 0' to fzf-launcher for_window rule
    - Removes i3's title bar and window border completely
    
    xterm config:
    - Changed -borderwidth 0 to -b 0 (internal border width)
    - Added -w 0 (border width, removes outer border)
    - Combined with i3's border pixel 0 for complete borderless appearance
    
    Result: Clean, borderless floating window with only the dark fzf
    interface visible, no xterm or i3 decorations.

--- home-modules/desktop/i3.nix
+++ home-modules/desktop/i3.nix
@@ -80,8 +80,8 @@
     bindsym $mod+Shift+Return exec ${pkgs.ghostty}/bin/ghostty --class=floating_terminal
     for_window [class="floating_terminal"] floating enable
 
-    # Floating fzf launcher window
-    for_window [instance="fzf-launcher"] floating enable
+    # Floating fzf launcher window - remove borders and title bar
+    for_window [instance="fzf-launcher"] floating enable, border pixel 0
 
     # Window management
     bindsym $mod+Shift+q kill
@@ -169,7 +169,7 @@
     bindsym $mod+Shift+9 move container to workspace $ws9
 
     # Project management keybindings (fzf-based switcher)
-    bindsym $mod+p exec ${pkgs.xterm}/bin/xterm -name fzf-launcher -fa 'Monospace' -fs 12 +sb -bg '#1e1e2e' -fg '#cdd6f4' -bc -borderwidth 0 -geometry 80x24 -e /etc/nixos/scripts/fzf-project-switcher.sh
+    bindsym $mod+p exec ${pkgs.xterm}/bin/xterm -name fzf-launcher -fa 'Monospace' -fs 12 +sb -bg '#1e1e2e' -fg '#cdd6f4' -bc -b 0 -w 0 -geometry 80x24 -e /etc/nixos/scripts/fzf-project-switcher.sh
     bindsym $mod+Shift+p exec i3pm clear
 
     # T022: Project-aware application launchers
